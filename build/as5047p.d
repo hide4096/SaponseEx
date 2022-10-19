dabfe3456299b74c541b2f20e57a24e9adbac42a
hide4096 <asouhide2002@gmail.com>
2022-10-19 10:53:35 +0900

    [fix]gitignoreを反映

--- build/as5047p.d
+++
@@ -1,4 +0,0 @@
-build/as5047p.o: Drivers/AS5047P/Src/as5047p.c \
- Drivers/AS5047P/Inc/as5047p_hal.h Drivers/AS5047P/Inc/as5047p.h
-Drivers/AS5047P/Inc/as5047p_hal.h:
-Drivers/AS5047P/Inc/as5047p.h:
--- build/startup_stm32f412cx.d
+++
@@ -1,1 +0,0 @@
-build/startup_stm32f412cx.o: startup_stm32f412cx.s
