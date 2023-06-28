0f331b11e9ed34e0214b09b0e72f9a58185b2dd9
hide4096 <asouhide2002@gmail.com>
2023-06-28 16:13:44 +0900

    [add]初期化処理追加

--- Main/Src/system.c
+++ Main/Src/system.c
@@ -8,11 +8,21 @@ void system_init(){
     }
     printf("%d\r\n",accelX_raw());
     */
-    HAL_GPIO_WritePin(GPIOB,GPIO_PIN_2,GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_0,GPIO_PIN_SET);
-    if(as5047p_init(&hspi1,GPIOB,GPIO_PIN_2) == -1){
-        printf("AS5047P init failed\r\n");
-        while(1);
+    AS5047P_Instance encL, encR;
+    AS5047P_Init(&encL, 0);
+    AS5047P_Init(&encR, 1);
+    uint8_t errcnt = 0;
+    while(AS5047P_ErrorPending(&encR) || AS5047P_ErrorPending(&encL)){
+        errcnt++;
+        if(errcnt >= 10) while(1);
+        AS5047P_Init(&encL, 0);
+        AS5047P_Init(&encR, 1);
+        HAL_Delay(100);
     }
+    AS5047P_SetZeroPosition(&encL);
+    AS5047P_SetZeroPosition(&encR);
+
+    printf("%d\r\n",AS5047P_ReadPosition(&encL,AS5047P_OPT_ENABLED));
+
     printf("Hello World!\r\n");
 }
