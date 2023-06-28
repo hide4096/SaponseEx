b24533e74251ed7a35b433b636ba504f43896ba7
hide4096 <asouhide2002@gmail.com>
2023-06-28 17:58:49 +0900

    [update]手動で読めるじゃん

--- Main/Src/system.c
+++ Main/Src/system.c
@@ -8,10 +8,9 @@ void system_init(){
     }
     printf("%d\r\n",accelX_raw());
     */
-    AS5047P_Instance encL, encR;
-    AS5047P_Init(&encL, 0);
-    AS5047P_Init(&encR, 1);
-    uint8_t errcnt = 0;
+    //AS5047P_Instance encL, encR;
+    //AS5047P_Init(&encL, 0);
+    //AS5047P_Init(&encR, 1);
     /*
     while(AS5047P_ErrorPending(&encR) || AS5047P_ErrorPending(&encL)){
         errcnt++;
@@ -21,11 +20,11 @@ void system_init(){
         HAL_Delay(100);
     }
     */
-    AS5047P_SetZeroPosition(&encL);
-    AS5047P_SetZeroPosition(&encR);
+    //AS5047P_SetZeroPosition(&encL);
+    //AS5047P_SetZeroPosition(&encR);
 
-    printf("%d\r\n",AS5047P_ReadPosition(&encL,AS5047P_OPT_ENABLED));
-    printf("%d\r\n",AS5047P_ReadPosition(&encR,AS5047P_OPT_ENABLED));
+    //printf("%d\r\n",AS5047P_ReadPosition(&encL,AS5047P_OPT_ENABLED));
+    //printf("%d\r\n",AS5047P_ReadPosition(&encR,AS5047P_OPT_ENABLED));
 
     printf("Hello World!\r\n");
     while(1){
@@ -34,7 +33,6 @@ void system_init(){
         send |= 1 << 15;
         HAL_GPIO_WritePin(GPIOA,GPIO_PIN_0,GPIO_PIN_RESET);
         HAL_SPI_TransmitReceive(&hspi1,(uint8_t*)&send,(uint8_t*)&recv,1,HAL_MAX_DELAY);
-        printf("%x\r\n",recv);
         HAL_GPIO_WritePin(GPIOA,GPIO_PIN_0,GPIO_PIN_SET);
 
         send = 0x3FFC;
@@ -45,7 +43,7 @@ void system_init(){
         HAL_GPIO_WritePin(GPIOA,GPIO_PIN_0,GPIO_PIN_RESET);
         HAL_SPI_TransmitReceive(&hspi1,(uint8_t*)&send,(uint8_t*)&recv,1,HAL_MAX_DELAY);
         HAL_GPIO_WritePin(GPIOA,GPIO_PIN_0,GPIO_PIN_SET);
-        printf("%x\r\n",recv);
+        printf("%d\r\n",recv&0x3FFF);
         HAL_Delay(500);
         //AS5047P_ReadWriteRaw(&encR, AS5047P_ANGLECOM, AS5047P_ACCESS_READ);
         //printf("%d\r\n",AS5047P_ReadWriteRaw(&encR,AS5047P_DIAAGC,AS5047P_ACCESS_READ));
